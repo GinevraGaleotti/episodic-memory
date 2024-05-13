59220537da32de10337f9bdfd3c395ad15152b98
GinevraGaleotti <ginevra.galeotti@virgilio.it>
2024-05-13 13:36:25 +0200

    diversi encoder VSLNet.py

--- NLQ/VSLNet/model/VSLNet.py
+++ NLQ/VSLNet/model/VSLNet.py
@@ -58,7 +58,16 @@ class VSLNet(nn.Module):
             dim=configs.dim,
             drop_rate=configs.drop_rate,
         )
-        self.feature_encoder = FeatureEncoder(
+        #PROVIAMO A FARE DUE ISTANZE SEPARATE PER ENCODER VIDEO E QUESRY
+        self.feature_encoder_query = FeatureEncoder(
+            dim=configs.dim,
+            num_heads=configs.num_heads,
+            kernel_size=7,
+            num_layers=4,
+            max_pos_len=configs.max_pos_len,
+            drop_rate=configs.drop_rate,
+        )
+          self.feature_encoder_video = FeatureEncoder(
             dim=configs.dim,
             num_heads=configs.num_heads,
             kernel_size=7,
@@ -123,8 +132,8 @@ class VSLNet(nn.Module):
         else:
             query_features = self.embedding_net(word_ids, char_ids)
 
-        query_features = self.feature_encoder(query_features, mask=q_mask)
-        video_features = self.feature_encoder(video_features, mask=v_mask)
+        query_features = self.feature_encoder_query(query_features, mask=q_mask)
+        video_features = self.feature_encoder_video(video_features, mask=v_mask)
         features = self.cq_attention(video_features, query_features, v_mask, q_mask)
         features = self.cq_concat(features, query_features, q_mask)
         h_score = self.highlight_layer(features, v_mask)
